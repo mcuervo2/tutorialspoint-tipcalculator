f67b383f71034456e55d65565470449f4f100dd9
mcuervo2 <71938763+mcuervo2@users.noreply.github.com>
2020-12-05 11:42:07 -0500

    initial changes

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// a = 2**(1/2)
+// let age= prompt ('what is your age', )
+// alert ('your age is ' + age)
+// console.log("your age is "+ age)
+// console.log(`your age is ${age}`)
+// let a = "hello ";
+
+// let age = prompt('what is your age');
+// output = (age <= 18) ? "child" : "grown up";
+// alert("you are a  " + output + "!");
+let a =  ('hello');
+let b =  ('hello');
+console.log(a===b);
