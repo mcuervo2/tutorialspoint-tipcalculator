c467bc6984b286447ce4a78e8504a69da00dcdf9
mcuervo2 <71938763+mcuervo2@users.noreply.github.com>
2020-12-06 13:36:36 -0500

    tis

--- script.js
+++ script.js
@@ -9,9 +9,20 @@
 // let age = prompt('what is your age');
 // output = (age <= 18) ? "child" : "grown up";
 // alert("you are a  " + output + "!");
-let a =  ('hello');
-let b =  ('hello');
-console.log(a===b);
-a = new String('hello');
-b = new String('hello');
-console.log(a===b);
+
+// let a =  ('hello');
+// let b =  ('hello');
+// console.log(a===b);
+// a = new String('hello');
+// b = new String('hello');
+// console.log(a===b);
+
+
+// let x = 'This this THis THIs is an example string';
+// let y = x.replace('an example', 'a sample');
+// y = y.replace(/this/ig, 'that')
+// console.log(y)
+
+let x = 'this is an example string';
+let y = x.charAt(5);
+console.log(y); //output i
